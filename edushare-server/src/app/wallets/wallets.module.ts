124bdf464f8d570ed8e9b8f48fdf2da81e38940e
Nhan Chau <chauhieunhan01@gmail.com>
2026-05-20 16:52:21 +0700

    feat: Add withdrawal request and review functionalities with associated DTOs and schema

--- edushare-server/src/app/wallets/wallets.module.ts
+++ edushare-server/src/app/wallets/wallets.module.ts
@@ -1,18 +1,25 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { Wallet, WalletSchema } from './schemas/wallet.schema'
 import { WalletsController } from './wallets.controller'
+import { AdminWithdrawalsController } from './admin-withdrawals.controller'
 import { WalletsService } from './wallets.service'
 import { Topup, TopupSchema } from './schemas/topup.schema'
+import { Withdrawal, WithdrawalSchema } from './schemas/withdrawal.schema'
+import { AuthModule } from '../auth/auth.module'
+import { PaginationUtilModule } from '../../common/utils/pagination-util/pagination-util.module'
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Wallet.name, schema: WalletSchema },
       { name: Topup.name, schema: TopupSchema },
+      { name: Withdrawal.name, schema: WithdrawalSchema },
     ]),
+    forwardRef(() => AuthModule),
+    PaginationUtilModule,
   ],
-  controllers: [WalletsController],
+  controllers: [WalletsController, AdminWithdrawalsController],
   providers: [WalletsService],
   exports: [MongooseModule, WalletsService], // Xuất ra để module khác sử dụng
 })
