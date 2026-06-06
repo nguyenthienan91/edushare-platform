7ff4a7de111daf09e03a2e1846ec096c463deacd
Nhan Chau <chauhieunhan01@gmail.com>
2026-06-06 10:20:00 +0700

    feat: add getMembers endpoint to retrieve group members with authorization checks

--- edushare-server/src/app/groups/groups.module.ts
+++ edushare-server/src/app/groups/groups.module.ts
@@ -6,10 +6,14 @@ import { Group, GroupSchema } from './entities/group.entity'
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module'
 import { UsersModule } from '../users/users.module'
 import { PaginationUtilModule } from '../../common/utils/pagination-util/pagination-util.module'
+import { Transaction, TransactionSchema } from '../transactions/schemas/transaction.schema'
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Group.name, schema: GroupSchema }]),
+    MongooseModule.forFeature([
+      { name: Group.name, schema: GroupSchema },
+      { name: Transaction.name, schema: TransactionSchema },
+    ]),
     forwardRef(() => UsersModule),
     PaginationUtilModule,
   ],
